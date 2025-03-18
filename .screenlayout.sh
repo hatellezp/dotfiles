9c4fe0e5c505747e1e89fbf1fbb6181403cb46d3
hatellezp <hatellezp@gmail.com>
2025-03-18 20:55:45 +0100

    Automatic update for day Tue Mar 18 08:55:45 PM CET 2025

--- .screenlayout.sh
+++ .screenlayout.sh
@@ -5,17 +5,25 @@ PRIMARY_OUTPUT="DP1-5"
 SECONDARY_OUTPUT="DP1-6"
 TERTIARY_OUTPUT="eDP1"
 
-# Check if HDMI0 is connected
+
+
+#  Check if house layout and usb-c 1 is connected
 if xrandr | grep "DP3-3 connected"; then
     PRIMARY_OUTPUT="DP3-3"
-    SECONDARY_OUTPUT="DP1-6"
-    TERTIARY_OUTPUT="eDP1"
 fi
+
+#  Check if house layout and usb-c 2 is connected
+if xrandr | grep "DP1-3 connected"; then
+    PRIMARY_OUTPUT="DP1-3"
+fi
+
+
+
 
 # Check if i3 is the running window manager
 if [ "$XDG_CURRENT_DESKTOP" = "i3" ] || [ "$DESKTOP_SESSION" = "i3" ] || pgrep -x "i3" > /dev/null; then
     # Set the primary, secondary, and tertiary monitors based on the detected layout
-    if xrandr | grep "DP3-3 connected"; then
+    if xrandr | grep "DP3-3 connected" || xrandr | grep "DP1-3 connected"; then
     	xrandr --output "$PRIMARY_OUTPUT" --auto --output  "$TERTIARY_OUTPUT" --off
     else 
         xrandr --output "$PRIMARY_OUTPUT" --primary
